90324770cdc58989c0cdbc936edb21921a662603
Satwik <satwiksri27@gmail.com>
2021-12-05 17:31:08 +0530

    Updated and Added to Colour and Player Settings

--- pacman/settings.py
+++ pacman/settings.py
@@ -1,14 +1,25 @@
 import os
 # Screen Settings
-WIDTH = 672
-HEIGHT = 864
+WIDTH = 560
+HEIGHT = 620
 FPS = 60
-PACMAN_ICON = "dependencies/icons/pacman_ico.png"
+PACMAN_ICON = "dependencies/images/pacman_ico.png"
 
 # Colour Settings
+## Colours
 BLACK = (0, 2, 2)
-START_TEXT_SIZES = {'START': 20, '1P': 20, 'CREATED': 20}
-START_TEXT_COLORS = {'START': (242, 178, 94), '1P': (18, 217, 253), 'CREATED': (206, 159, 206)}
+RED = (255, 110, 137)
+WHITE = (255, 255, 255)
+GREY = (86, 96, 120)
+
+START_TEXT_SIZES = {'START': 20, '1P': 20, 'CREATED': 20, 'HIGH_SCORE': 20}
+START_TEXT_COLORS = {
+                    'START': (242, 178, 94), 
+                    '1P': (18, 217, 253), 
+                    'CREATED': (206, 159, 206), 
+                    'HIGH_SCORE': (255, 255, 255)
+                    }
+
 START_FONT = "dependencies/fonts/emulogic.ttf"
 
 
